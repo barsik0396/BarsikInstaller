89bf53850ee9ee199b8674c3fc3398d9ce852eb2
barsik0396 <Barsik0396@outlook.com>
2025-12-27 16:00:23 +0200

    Update versionInstaller.bat

--- versions/0.1.0-alpha/versionInstaller.bat
+++ versions/0.1.0-alpha/versionInstaller.bat
@@ -2,3 +2,14 @@
 chcp 65001
 echo O_O А как это работает?
 pause
+:: я вообще не знаю что сюда добавить
+:: что первое придумал то и написал
+
+:AAA
+set /p aaa=aaa 
+if "%aaa%" == "aaa" (
+  echo %aaa% = aaa!
+) else (
+  echo ERROROROROWJIERJNWHFEJNWEUHIJVNBUIEFGVNBUERJGNVBUWfjnvbrhdjfnHJnIUHJFNSDUIFJOWENFuhwjsednvhjv!
+  goto AAA
+)
